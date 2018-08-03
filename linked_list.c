5f9c74f8bf63b7136b0aae5c66464e994c57fd9a
gabrielmougard <gabriel.mougard@gmail.com>
2018-08-03 17:57:43 +0200

    features

--- linked_list.c
+++ linked_list.c
@@ -457,6 +457,35 @@ void swapNodes(struct Node **head_ref, int x, int y)
    currX->next  = temp;
 }
 
+/*Function to swap two integers at addresses a and b */
+void swap(int *a, int *b);
+
+/* Function to pairwise swap elements of a linked list */
+void pairWiseSwap(struct Node *head)
+{
+    struct Node *temp = head;
+
+    /* Traverse further only if there are at-least two nodes left */
+    while (temp != NULL && temp->next != NULL)
+    {
+        /* Swap data of node with its next node's data */
+        swap(&temp->data, &temp->next->data);
+
+        /* Move temp by 2 for the next pair */
+        temp = temp->next->next;
+    }
+}
+
+/* UTILITY FUNCTIONS */
+/* Function to swap two integers */
+void swap(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+} 
+
 //program to create a simple linked list with 6 nodes
 int main(void) {
 
